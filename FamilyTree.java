76c3e490c1ea9f939df9c8375c48e3bc0ce97654
Haseeb08 <yourhaseeb08@gmail.com>
2020-02-11 00:17:31 +0530

    8th Example

---
+++ FamilyTree.java
@@ -0,0 +1,69 @@
+interface grandFather {
+	public void grandFatherDetail();
+}
+interface grandMother {
+	public void grandMotherDetail();
+}
+interface Mother {
+	public void motherDetail();	
+}
+class Father implements grandFather,grandMother{
+	private String name;
+	private int age;
+	
+	
+	public void fatherDetail() {
+			System.out.println("Father: name "+name+" Age: "+age);
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	public void grandFatherDetail() {
+		System.out.println("Grandfather:\t Name: Mike");
+	}
+	public void grandMotherDetail() {
+		System.out.println("Grandmother:\t Name: Jane");
+	}
+
+}
+
+class Child extends Father implements Mother{
+
+	public void ChildDetail() {
+		System.out.println("Child: Name:"+getName()+" Age:"+getAge());
+	}
+
+	@Override
+	public void motherDetail() {
+		System.out.println("Mother:\t Name: Rachel");
+		
+	}
+}
+
+public class FamilyTree {
+	
+	public static void main() {
+		Child child=new Child();
+		child.setName("Emma");
+		child.setAge(10);
+		Father father=new Father();
+		father.setName("Ross");
+		father.setAge(35);		
+		child.ChildDetail();
+		father.fatherDetail();
+		child.motherDetail();
+		father.grandFatherDetail();
+		father.grandMotherDetail();
+		
+	}
+}
