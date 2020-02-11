6986910f11d8ac8828762ac069b26248a53a112b
Haseeb08 <yourhaseeb08@gmail.com>
2020-02-11 12:28:21 +0530

    Example 6 : Problem Statement

--- Restaurant.java
+++ Restaurant.java
@@ -4,7 +4,6 @@
 abstract class Meal{
     abstract void displayMenu();
 }
-
 class Pizza extends Meal{
 
     @Override
@@ -16,9 +15,7 @@ class Pizza extends Meal{
             System.out.println("**NON-VEG\nChicken Peri peri\nChicken Tikka\nChicken Barbeque");
     }
 }
-class Burger extends Meal{
-
-    
+class Burger extends Meal{   
     void displayMenu() {
         System.out.println("BURGER");
             System.out.println("\nPaneer tikki\nAloo Tikki\nChicken Patti\nMaharaja");
@@ -26,10 +23,7 @@ class Burger extends Meal{
 
     }
 }
-
-
 interface Bottle{
-
      void drinkTypes();
 }
 class Drink implements Bottle{
@@ -37,7 +31,6 @@ class Drink implements Bottle{
     public void drinkTypes() {
         System.out.println("**Drinks**\nCoke\nSprite\nMazza\n");
     }
-
 }
 public class Restaurant {
 	 
@@ -47,7 +40,6 @@ public class Restaurant {
 	Meal burgerMeal=new Burger();
 	burgerMeal.displayMenu();
 	Drink drink=new Drink();
-	drink.drinkTypes();
-  
+	drink.drinkTypes(); 
     }
 }
